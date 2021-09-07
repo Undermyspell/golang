77f0fbfd73b9296724ea65ff80a228010a9037bd
Michael Kaiser <michael.kaiser@conplement.de>
2021-09-07 15:31:23 +0200

    adds go routines and channel for stringify

--- RestApi/albums/route.handler.go
+++ RestApi/albums/route.handler.go
@@ -2,7 +2,9 @@ package albumspkg
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +13,11 @@ var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Alber Vaillant", Artist: "The Streetboy", Price: 39.99},
+	{ID: "5", Title: "Terry Hills", Artist: "US5", Price: 39.99},
+	{ID: "6", Title: "Christopher Wonder", Artist: "Westlife", Price: 39.99},
+	{ID: "7", Title: "Chuck Bass", Artist: "Shaggy", Price: 39.99},
+	{ID: "8", Title: "Dexter Morgan", Artist: "Tobi van Buren", Price: 39.99},
 }
 
 func GetAlbums(c *gin.Context) {
@@ -39,6 +46,22 @@ func SetAlbumTitle(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func StringifyAlbums(c *gin.Context) {
+	strings := []string{}
+
+	messages := make(chan string)
+
+	for _, album := range albums {
+		go stringify(album, messages)
+	}
+
+	for i := 0; i < len(albums); i++ {
+		strings = append(strings, <-messages)
+	}
+
+	c.IndentedJSON(http.StatusOK, strings)
+}
+
 func findById(a []Album, id string) *Album {
 	for i, album := range a {
 		if album.ID == id {
@@ -46,4 +69,11 @@ func findById(a []Album, id string) *Album {
 		}
 	}
 	return nil
+}
+
+func stringify(a Album, c chan string) {
+	elements := []string{a.ID, a.Title, a.Artist, fmt.Sprintf("%f", a.Price)}
+	s := strings.Join(elements, "|")
+
+	c <- s
 }
--- RestApi/main.go
+++ RestApi/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", albumspkg.GetAlbums)
+	router.GET("/albums/stringify", albumspkg.StringifyAlbums)
 	router.PUT("/album/:id/settitle", albumspkg.SetAlbumTitle)
 
 	router.Run("localhost:4711")
